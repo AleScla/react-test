a9082aa3bab6e8848505015bb4c1fec9eef0902d
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-10 15:34:25 +0100

    inserita logica per alternare la X alla O

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import Player from './components/Player'
+import {useState} from 'react';
+import Player from './components/Player';
+import GameBoard from './components/GameBoard'
 function App() {
+  const [activePlayer, setActivePlayer ] = useState('X');
+  function checkSelectedSquare(){
+    // qui, nuovamente, creo una nuova variabile per non andare a modificare il valore originale di activePlayer
+    setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
+
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="player 1" symbol="x"/>
-          <Player name="player 2" symbol="o"/>
+        <ol id="players" className='highlight-player'>
+          <Player name="player 1" symbol="x" isActive={activePlayer === 'X'}/>
+          <Player name="player 2" symbol="o" isActive={activePlayer === 'O'}/>
         </ol>
+        <GameBoard activePlayerSymbol={activePlayer} onSelectedSquare={checkSelectedSquare}/>
       </div>
     </main>
   )
