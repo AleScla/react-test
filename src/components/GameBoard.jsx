63821fce78689eb5f1eeb8c87928af7560d85a56
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-10 18:17:55 +0100

    Creati component GameBoard e Log, ora è impossibile cliccare più volte sullo stesso button.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,30 +1,38 @@
-import {useState} from 'react';
+
 const board = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
-export default function GameBoard({ onSelectedSquare, activePlayerSymbol }){
-    const [gameBoard, setGameBoard] = useState(board)
-
-    function handleSelectedSquare(rowIndex, colIndex){
-        setGameBoard((initialBoard)=>{
-            // questo serve a non modificare il valore originale dell'array perchè potrebbe causare dei bug
-            // best practice è creare un nuovo array ad ogni click su casella
-            const updatedBoard = [...initialBoard.map((innerArray) => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard
-        })
-        onSelectedSquare()
+export default function GameBoard({ onSelectedSquare, turns }){
+    let gameBoard = board;
+    for(const turn of turns){
+        const { square, player } = turn;
+        const { row, col } = square;
+        gameBoard[row][col] = player;
     }
+    // commentato perchè gestire la logica interamente qui non ci permetteva di avere dei log della partita nella pagina principale.
+    // const [gameBoard, setGameBoard] = useState(board)
+    // function handleSelectedSquare(rowIndex, colIndex){
+    //     setGameBoard((initialBoard)=>{
+    //         // questo serve a non modificare il valore originale dell'array perchè potrebbe causare dei bug
+    //         // best practice è creare un nuovo array ad ogni click su casella
+    //         const updatedBoard = [...initialBoard.map((innerArray) => [...innerArray])]
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard
+    //     })
+    //     onSelectedSquare()
+    // }
     return(
         <ol id="game-board">
             {gameBoard.map((row, rowIndex)=> 
                 <li key={rowIndex}>
                     <ol>
-                        {row.map((square, squareIndex) => 
+                        {row.map((singleSquare, squareIndex) => 
                             <li key={squareIndex}>
-                                <button onClick={() => handleSelectedSquare(rowIndex, squareIndex)}>{square}</button>
+                                <button onClick={() => onSelectedSquare(rowIndex, squareIndex)} disabled={singleSquare !== null}>
+                                    {singleSquare}
+                                </button>
                             </li>
                         )}
                     </ol>
