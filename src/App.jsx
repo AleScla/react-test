c8256c9745389a479ba6a0b5240efe7d14b63f9a
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-10 18:19:03 +0100

    modificata logica sul funzionamento del gioco in modo da poterne sfruttare i valori in ogni component. Inizializzata logica di vittoria/pareggio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import {useState} from 'react';
 import Player from './components/Player';
-import GameBoard from './components/GameBoard'
+import GameBoard from './components/GameBoard';
+import Log from './components/Log';
+import { WINNING_COMBINATIONS } from './winning-combinations';
+
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer ] = useState('X');
-  function checkSelectedSquare(){
+  for (const combination of WINNING_COMBINATIONS){
+    
+  }
+
+  function checkSelectedSquare(rowIndex, colIndex){
     // qui, nuovamente, creo una nuova variabile per non andare a modificare il valore originale di activePlayer
     setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
-
+    setGameTurns( prevGameTurns => {
+      let currentPlayer = 'X'
+      if(prevGameTurns.length > 0 && prevGameTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [{square : { row: rowIndex, col: colIndex}, player: activePlayer},  ...prevGameTurns]
+      
+      return updatedTurns
+    });
   }
   return (
     <main>
@@ -15,8 +31,9 @@ function App() {
           <Player name="player 1" symbol="x" isActive={activePlayer === 'X'}/>
           <Player name="player 2" symbol="o" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard activePlayerSymbol={activePlayer} onSelectedSquare={checkSelectedSquare}/>
+        <GameBoard onSelectedSquare={checkSelectedSquare} turns={gameTurns}/>
       </div>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
